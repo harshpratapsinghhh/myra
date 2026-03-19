0a5f3a731ab52c315c129bf9df40bbfd17053057
harshpratapsinghhh <harshsuryavanshi169@gmail.com>
2026-03-20 01:45:34 +0530

    Config updated with assistant_config.py and setting.py

---
+++ config/assistant_config.py
@@ -0,0 +1,24 @@
+ASSISTANT_NAME = "myra"
+
+WAKE_WORDS = [
+    "hey myra",
+    "myra",
+    "wake up myra",
+    "hey mayara",
+    "hey myara",
+    "he mayara",
+    "he may"
+]
+
+SLEEP_WORDS = [
+    "go to sleep",
+    "sleep myra"
+]
+
+EXIT_WORDS = [
+    "shutdown",
+    "exit",
+    "bye myra"
+]
+
+VOICE_TONE = "professional"   # or "casual"
