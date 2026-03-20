3806e09f65df12738dd44909b2103df3223b307a
harshpratapsinghhh <harshsuryavanshi169@gmail.com>
2026-03-21 01:06:15 +0530

    run.py have same main.py but in text format which helps to do testing of code fast

---
+++ run.py
@@ -0,0 +1,21 @@
+from core.brain import process
+from output.speaker import speak
+
+def text_mode():
+    speak("Text mode activated.")
+
+    while True:
+        command = input("You: ").lower().strip()
+
+        if not command:
+            continue
+
+        if command in ["exit", "quit", "bye"]:
+            speak("Shutting down text mode.")
+            break
+
+        process(command)
+
+
+if __name__ == "__main__":
+    text_mode()
