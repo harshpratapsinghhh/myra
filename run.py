1d4adf8f89324a4147627b3b39ae1a11ef4cb123
harshpratapsinghhh <harshsuryavanshi169@gmail.com>
2026-03-23 02:17:17 +0530

    run updated with trigger in it

--- run.py
+++ run.py
@@ -1,11 +1,18 @@
 from core.brain import process
 from output.speaker import speak
+from actions.music_actions import pause_music_action
 
 def text_mode():
     speak("Text mode activated.")
 
     while True:
         command = input("You: ").lower().strip()
+
+        # HOTWORD INTERRUPT
+        if command.strip() in ["myra", "hey myra"]:
+            pause_music_action()  
+            speak("Yes?")
+            continue
 
         if not command:
             continue
@@ -20,3 +27,5 @@ if __name__ == "__main__":
     text_mode()
 
 # play music udte teer jamaiya ke
+# What MYRA is thinking right now -- memory.json
+# What MYRA has learned over time -- history.json
